2c49183e7986bc7b74dd856b27376a9af6e8837e
Kartel Maksym <kartmax1804@gmail.com>
2022-11-29 20:24:53 +0100

    navbar-deep

--- js/common.js
+++ js/common.js
@@ -7,29 +7,31 @@ document.addEventListener('DOMContentLoaded', function() {
    //========================================================
    //==================== Модальное окно ====================
    //========================================================
-   MicroModal.init({
-      openTrigger : 'data-modal-open',
-      closeTrigger : 'data-modal-close',
-      disableScroll : true,
-      // disableFocus : true,
-      awaitOpenAnimation : true,
-      awaitCloseAnimation : true,
-
-      onShow : function() {
-         // Устраняю сдвиг-дергание контента в момент открытия модального окна
-         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) == false) {
-            document.body.classList.add('js-open-modal');
-            document.querySelector('.modal__overlay').classList.add('js-open-modal');
-         };
-      },
-      onClose: function() {
-         // Устраняю сдвиг-дергание контента в момент закрытия модального окна
-         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) == false) {
-            document.body.classList.remove('js-open-modal');
-            document.querySelector('.modal__overlay').classList.remove('js-open-modal');
-         };
-      }
-   })
+   if(document.querySelector('.micromodal-slide')) {
+      MicroModal.init({
+         openTrigger : 'data-modal-open',
+         closeTrigger : 'data-modal-close',
+         disableScroll : true,
+         // disableFocus : true,
+         awaitOpenAnimation : true,
+         awaitCloseAnimation : true,
+   
+         onShow : function() {
+            // Устраняю сдвиг-дергание контента в момент открытия модального окна
+            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) == false) {
+               document.body.classList.add('js-open-modal');
+               document.querySelector('.modal__overlay').classList.add('js-open-modal');
+            };
+         },
+         onClose: function() {
+            // Устраняю сдвиг-дергание контента в момент закрытия модального окна
+            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) == false) {
+               document.body.classList.remove('js-open-modal');
+               document.querySelector('.modal__overlay').classList.remove('js-open-modal');
+            };
+         }
+      })
+   }
 
    // Прокидываю значение data-form из кнопки открытия модального окна в скрытый input формы
    // для дальнейшей идентификации формы при обработке запросов на сервере
@@ -147,77 +149,84 @@ document.addEventListener('DOMContentLoaded', function() {
    //=====================================================
 
    // home-slider
-   const homeSlider = new Swiper('.home-slider', {
-      speed : 800,
-      effect : 'fade',
-      centeredSlides : true,
-      // loop : true,
-      pagination : {
-         el : '.home-slider__pagination',
-         type : 'custom',
-         renderCustom  : function(swiper, current, total) {
-            let indTotal = total >= 10 ? total : `0${total}`
-            let indCurrent = current >= 10 ? current : `0${current}`
-            return `<b>${indCurrent }</b><span></span>${indTotal}`
-         }
-      },
-      scrollbar : {
-         el : '.home-slider__scrollbar',
-         draggable : true
-      },
-      navigation : {
-         prevEl : '.home-slider__prev',
-         nextEl : '.home-slider__next'
-      },
-      keyboard : {
-         enabled : true,
-         onlyInViewport : false
-      },
-      runCallbacksOnInit : true
-   });
+   if(document.querySelector('.home-slider')) {
+      const homeSlider = new Swiper('.home-slider', {
+         speed : 800,
+         effect : 'fade',
+         centeredSlides : true,
+         // loop : true,
+         pagination : {
+            el : '.home-slider__pagination',
+            type : 'custom',
+            renderCustom  : function(swiper, current, total) {
+               let indTotal = total >= 10 ? total : `0${total}`
+               let indCurrent = current >= 10 ? current : `0${current}`
+               return `<b>${indCurrent }</b><span></span>${indTotal}`
+            }
+         },
+         scrollbar : {
+            el : '.home-slider__scrollbar',
+            draggable : true
+         },
+         navigation : {
+            prevEl : '.home-slider__prev',
+            nextEl : '.home-slider__next'
+         },
+         keyboard : {
+            enabled : true,
+            onlyInViewport : false
+         },
+         runCallbacksOnInit : true
+      });
+   }
 
    // advantages-slider
-   const advSlider = new Swiper('.advantages-slider', {
-      effect : 'fade',
-      speed : 1400,
-      autoplay : {
-         delay : 5000,
-         disableOnInteraction : false,
-      },
-   });
+   if(document.querySelector('.advantages-slider')) {
+      const advSlider = new Swiper('.advantages-slider', {
+         effect : 'fade',
+         speed : 1400,
+         autoplay : {
+            delay : 5000,
+            disableOnInteraction : false,
+         },
+      });
+   }
 
    // our-blog-slider
-   const ourBlogSlider = new Swiper('.our-blog-slider', {
-      speed : 800,
-      lazy: true,
-      keyboard : {
-         enabled : true,
-         onlyInViewport : false
-      },
-      spaceBetween: 20,
-
-      breakpoints: {
-         768: {
-            slidesPerView: 2,
+   if(document.querySelector('.our-blog-slider')) {
+      const ourBlogSlider = new Swiper('.our-blog-slider', {
+         speed : 800,
+         lazy: true,
+         keyboard : {
+            enabled : true,
+            onlyInViewport : false
          },
-         992: {
-            slidesPerView: 3,
-         }
-      },
+         spaceBetween: 20,
+   
+         breakpoints: {
+            768: {
+               slidesPerView: 2,
+            },
+            992: {
+               slidesPerView: 3,
+            }
+         },
+   
+         pagination: {
+            el: '.our-blog-slider__pagination',
+            clickable: true,
+            bulletClass: 'our-blog-slider__bullet',
+            bulletActiveClass: 'our-blog-slider__bullet-active',
+          },
+        
+      });
 
-      pagination: {
-         el: '.our-blog-slider__pagination',
-         clickable: true,
-         bulletClass: 'our-blog-slider__bullet',
-         bulletActiveClass: 'our-blog-slider__bullet-active',
-       },
-     
-   });
+      // одинаковая высота слайдов
+      const wrapSlides = document.querySelector('.our-blog-slider__wrapper');
+      const hWrapSlides = wrapSlides.clientHeight;
+      wrapSlides.style.cssText += `--height: ${hWrapSlides}px`;
+   }
 
-   // одинаковая высота слайдов
-   const wrapSlides = document.querySelector('.our-blog-slider__wrapper');
-   const hWrapSlides = wrapSlides.clientHeight;
-   wrapSlides.style.cssText += `--height: ${hWrapSlides}px`;
 
 
    //==================================================================================
@@ -264,6 +273,31 @@ document.addEventListener('DOMContentLoaded', function() {
             left: 0,
             behavior: 'smooth',
          })
+
+         // function scrollToTop(duration) {
+         //    const yStart   = window.scrollY;
+         //    let startAnimationTime = null;
+
+         //    function step (time) {
+         //       if(!startAnimationTime) {
+         //          startAnimationTime = time;
+         //       } 
+               
+         //       const progress = Math.min((time - startAnimationTime) / duration, 1);
+         //       const y = yStart - (progress * yStart);
+
+         //       window.scrollTo(0, -y);
+
+         //       if (y > 0) {
+         //          requestAnimationFrame(step)
+         //       }
+         //    }
+         //    requestAnimationFrame(step)
+         // }
+         // scrollToTop(1000);
+
+         
+
       });
 
    };
